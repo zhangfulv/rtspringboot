b2f5573d9934f97040ff0da8f8d450fe7e12d89a
zhangfulv <63481858+zhangfulv@users.noreply.github.com>
2020-12-02 11:00:57 +0800

    基础模块

---
+++ rt-controller/src/main/java/com/routine/spbcore/SpbcoreApplication.java
@@ -0,0 +1,13 @@
+package com.routine.spbcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbcoreApplication.class, args);
+	}
+
+}
---
+++ rt-controller/src/test/java/com/routine/spbcore/SpbcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.routine.spbcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
